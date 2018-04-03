55b669fb7490ec0d034a1701a381645fccdc6c0d
Ed Babcock <greenyouse@hotmail.com>
2018-04-03 14:23:06 -0500

    style: using standard emacs conventions in config.el

--- resources/config.el
+++ resources/config.el
@@ -1,11 +1,13 @@
-;;; Anyone following along, evaluate both of these sexprs with C-c md and then
-;;; call org-publish within THIS FILE.
-
+;;; package -- Summary:
+;;; Config for my blog
+;;;
+;;; Commentary:
 ;;; For including ClojureScript, add #+HTML: with a <script> pointing to the
 ;;;  output js file for that page
 
 ;; set html options here (they'll be added to all output files)
-(setq org-html-preamble "<nav class=\"navbar navbar-default\">
+;;; Code:
+(defvar org-html-preamble "<nav class=\"navbar navbar-default\">
     <div class=\"container-fluid\">
       <div class=\"navbar-header\">
         <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#main-navbar\">
@@ -24,40 +26,42 @@
         </ul>
       </div>
     </div>
-  </nav>"
+  </nav>")
 
-      org-html-postamble "<footer> <hr />
+(defvar org-html-postamble "<footer> <hr />
                          <p><a href='http://creativecommons.org/licenses/by-nc-sa/3.0/' target='_blank'>©copyright 2015</a>, by <a href='/about.html'>Ed Babcock</a> (^_^)v</p>
                          </footer>
                          <script type='text/javascript' src='js/jquery.min.js'></script>
-                         <script type='text/javascript' src='js/bootstrap.min.js'></script>"
-      org-html-head "<meta name='viewport' content='width=device-width' initial-scale=1>
+                         <script type='text/javascript' src='js/bootstrap.min.js'></script>")
+(defvar org-html-head "<meta name='viewport' content='width=device-width' initial-scale=1>
                      <link rel='shortcut icon' href='/img/favicon.ico' />
                      <link rel='stylesheet' href='/css/bootstrap.min.css' type='text/css'/>
-                     <link rel='stylesheet' href='/css/blog.css' type='text/css'/>"
-      user-full-name "Ed Babcock (greenyouse)")
+                     <link rel='stylesheet' href='/css/blog.css' type='text/css'/>")
+(defvar user-full-name "Ed Babcock (greenyouse)")
 
-(setq org-publish-project-alist
-      '(("orgfiles"
-         :base-directory "./www/"
-         :base-extension "org\\|md"
-         :recursive t
-         :publishing-directory "./public/"
-         :section-numbers nil
-         :auto-sitemap nil
-         :sitemap-title "Archive"
-         :sitemap-filename "archive.org"
-         :sitemap-sort-files "chronologically"
-         :description "Hacks, projects, and general butt-kicking with Ed (greenyouse)"
-         :keywords "Clojure, ClojureScript, greenyouse, LISP"
-         :with-toc nil
-         :publishing-function org-html-publish-to-html
-         :html-html5-fancy t
-         )
-        ("org-static"
-         :base-directory "./www/"
-         :base-extension "css\\|js\\|png\\|svg\\|jpg\\|gif\\|pdf"
-         :publishing-directory "public/"
-         :recursive t
-         :publishing-function org-publish-attachment)
-        ("pwa" :components ("orgfiles" "org-static"))))
+(defvar org-publish-project-alist
+  '(("orgfiles"
+     :base-directory "./www/"
+     :base-extension "org\\|md"
+     :recursive t
+     :publishing-directory "./public/"
+     :section-numbers nil
+     :auto-sitemap nil
+     :sitemap-title "Archive"
+     :sitemap-filename "archive.org"
+     :sitemap-sort-files "chronologically"
+     :description "Hacks, projects, and general butt-kicking with Ed (greenyouse)"
+     :keywords "Clojure, ClojureScript, greenyouse, LISP"
+     :with-toc nil
+     :publishing-function org-html-publish-to-html
+     :html-html5-fancy t
+     )
+    ("org-static"
+     :base-directory "./www/"
+     :base-extension "css\\|js\\|png\\|svg\\|jpg\\|gif\\|pdf"
+     :publishing-directory "public/"
+     :recursive t
+     :publishing-function org-publish-attachment)
+    ("pwa" :components ("orgfiles" "org-static"))))
+
+;;; config.el ends here
